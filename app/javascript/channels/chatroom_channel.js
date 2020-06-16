0dd79e7caec19c6170d5fd954f26a7267dad5bc5
Shubham Agarwal <shubham.agarwal@navi.com>
2020-06-16 18:40:53 +0530

    render a newly created message

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -11,5 +11,6 @@ consumer.subscriptions.create("ChatroomChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
+    document.getElementById('message-container').append(data.mod_message)
   }
 });

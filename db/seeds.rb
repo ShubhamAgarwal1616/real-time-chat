9a2822a973d6cb9ea1c4dc87f9cb21c58d922505
Shubham Agarwal <shubham.agarwal@navi.com>
2020-06-16 12:34:32 +0530

    add message model and migration for it and display actual mesages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "shubham", password: "password")
+Message.create(body: "Hi this is a new message", user: User.last)
